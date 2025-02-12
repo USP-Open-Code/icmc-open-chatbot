3cb6bac5ead6ef2088ff619b91fd4c030ae967a4
Gustavo Ortega <gustavo_ortega@usp.br>
2025-02-12 14:05:52 -0300

    [bugfix] refactoring main

--- src/main.py
+++ src/main.py
@@ -1,21 +1,22 @@
-from contextlib import asynccontextmanager
 from fastapi import FastAPI
+
+from src.infrastructure.database import MongoDB
 from src.api.routes import chat_router
-from src.database.mongodb.connector import MongoDB
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    # Startup
-    app.mongodb = MongoDB()
-    await app.mongodb.connect(db_name="crag_app")
-    yield
-    # Shutdown
-    await app.mongodb.close()
+from src.infrastructure.config import settings
+from src.infrastructure.config.llm import LLM
 
 
 def create_app():
-    # app = FastAPI(lifespan=lifespan)
     app = FastAPI()
+
+    # defining API variables
+    app.database = MongoDB(db_name=settings.MONGO_DB)
+    app.llm = LLM(model_name=settings.MODEL)
+
+    # app.vector_store = ChromaDB()
+    # app.llm = choose_model(model_name=settings.MODEL)
+
+    # including routes
     app.include_router(chat_router)
+
     return app
