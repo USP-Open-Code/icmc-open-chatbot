9061b12976b77d6054b72b2d4a218971acfb2f0c
Gustavo Ortega <gustavo_ortega@usp.br>
2025-03-22 23:48:41 -0300

    [feat] add collection name to file metadata class

--- src/api/models/files.py
+++ src/api/models/files.py
@@ -1,7 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional, Dict, Any
+
+from src.infrastructure.config import settings
 
 
 class FileMetadata(BaseModel):
-    collection_name: str
+    collection_name: str = Field(default=settings.INDEX_NAME)
     metadatas: Optional[Dict[str, Any]] = None
