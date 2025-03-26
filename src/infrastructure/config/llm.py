a26449297ef998ef62b103954a8ca3feab73733b
Gustavo Ortega <gustavo_ortega@usp.br>
2025-03-26 10:59:54 -0300

    [feat] add ping model before the app starts

--- src/infrastructure/config/llm.py
+++ src/infrastructure/config/llm.py
@@ -7,26 +7,33 @@ from langchain_openai import ChatOpenAI
 class LLM:
     def __new__(cls):
         model_name = settings.MODEL
-        try:
-            if model_name == "ollama":
-                return ChatOllama(
-                    model=settings.MODEL_NAME,
-                    base_url=settings.MODEL_URL,
-                    temperature=settings.MODEL_TEMPERATURE
-                )
 
-            elif model_name == "openai":
-                return ChatOpenAI(
-                    model=settings.MODEL_NAME,
-                    base_url=settings.MODEL_URL,
-                    temperature=settings.MODEL_TEMPERATURE,
-                    api_key=settings.MODEL_API_KEY
-                )
-            # More models can be added here
+        if model_name == "ollama":
+            model = ChatOllama(
+                model=settings.MODEL_NAME,
+                base_url=settings.MODEL_URL,
+                temperature=settings.MODEL_TEMPERATURE
+            )
 
+        elif model_name == "openai":
+            model = ChatOpenAI(
+                model=settings.MODEL_NAME,
+                base_url=settings.MODEL_URL,
+                temperature=settings.MODEL_TEMPERATURE,
+                api_key=settings.MODEL_API_KEY
+            )
+        # More models can be added here
+        else:
             raise ValueError(f"Model {model_name} not supported")
 
+        _ = cls.ping_model(model)
+        return model
+
+    @staticmethod
+    def ping_model(model: ChatOllama | ChatOpenAI) -> None:
+        try:
+            model.invoke("Hello, world!")
         except Exception as e:
             raise ValueError(
-                f"Problem instantiating the model {model_name}: {e}"
+                f"Problem pinging the model: {e}"
             )
