3b0b37e9b6cd5c9f8271a809526c783c638ff642
Gustavo Ortega <gustavo_ortega@usp.br>
2025-03-30 19:56:47 -0300

    [bugfix] fix PORT parameter to use .env value

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
 import uvicorn
+import os
+
 from src.main import create_app
 
-app = create_app()
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    app = create_app()
+    PORT = os.getenv("API_PORT", 9876)
+
+    uvicorn.run(app, host="0.0.0.0", port=int(PORT))
