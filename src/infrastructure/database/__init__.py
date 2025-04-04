e63dd880c673d574ba3fa8f99a16d5a53578880a
Gustavo Ortega <gustavo_ortega@usp.br>
2025-04-04 09:27:47 -0300

    [bugfix] remove legacy import

--- src/infrastructure/database/__init__.py
+++ src/infrastructure/database/__init__.py
@@ -2,7 +2,6 @@ from .mongodb.connector import MongoDB
 from .mongodb.utils import get_service_details, get_user_details, block_user
 
 from .chromadb.connector import ChromaDB
-from .chromadb.utils import retriever_tool
 
 __all__ = [
     "MongoDB",
@@ -10,5 +9,4 @@ __all__ = [
     "get_user_details",
     "block_user",
     "ChromaDB",
-    "retriever_tool"
 ]
